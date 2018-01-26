418dbbfefd48c03020d71cef1b1e85a772d8f470
merlink <merlink@gmx.de>
2018-01-26 17:24:20 +0100

    Work on Travis to get minidlna up

--- fs/dlna/dlnafs.py
+++ fs/dlna/dlnafs.py
@@ -119,8 +119,8 @@ class DLNAFS(FS):
         if _path in [u'', u'.', u'/', u'./']:
             for device in self.devices:
                 if six.PY2:
-                    device = device
-                outlist.append(u''.join(device))
+                    device = u''.join(str(device))
+                outlist.append(device)
             return outlist
 
         pathiter = iteratepath(_path)
